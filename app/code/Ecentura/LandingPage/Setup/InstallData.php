23b319290dcaa6f7d602ab94fe6f54833d68bb75
dungla <anhdung1301@gmail.com>
2021-03-15 17:40:16 +0700

    css module landingpage

--- app/code/Ecentura/LandingPage/Setup/InstallData.php
+++ app/code/Ecentura/LandingPage/Setup/InstallData.php
@@ -29,7 +29,87 @@ class InstallData implements InstallDataInterface
         ModuleContextInterface $context
     )
     {
-        $content = '<div class="content-below" >
+        $banner = '
+        <div class="ad_content_banner">
+    <div class="main-banner">
+        <div class="main-category">
+            <div class="text"><h2>Katoenen tassen bedrukken</h2>
+                <ul>
+                    <li>Al vanaf <strong>10</strong> stuks voor <strong>€ 45<span>,67</span></strong></li>
+                    <li>Leverbaar binnen <strong><span class="delivery-days">4</span> werkdagen</strong></li>
+                </ul>
+                <a href="https://www.pinkcube.nl/tassen-bedrukken/katoenen-tassen.html" class="btn btn-success btn-xl">Katoenen
+                    tassen</a></div>
+            <img src="https://www.pinkcube.nl/media/homepage/katoenen-tassen.png" title="Katoenen tassen bedrukken">
+        </div>
+        <div class="mhmr">
+
+            <div class="head">
+                <a href="https://www.pinkcube.nl/moeiteloos-het-mooiste-resultaat"
+                   title="Moeiteloos het mooiste resultaat" target="_blank">Moeiteloos het <strong>mooiste
+                        resultaat</strong></a>
+            </div>
+
+            <div class="row usp">
+                <span class="usp-head">Gemakkelijk <strong>online bestellen</strong></span>
+            </div>
+
+            <div class="row usp">
+                <span class="usp-head"><strong>Gratis</strong> digitaal ontwerp</span>
+            </div>
+
+            <div class="row usp">
+                <span class="usp-head">Snelle levering en service</span>
+            </div>
+
+        </div>
+    </div>
+    <div class="row main-subcats">
+        <div class="ad_related_pro">
+            <div class="raleted_pro_general">
+                <div class="raleted_pro_detailds"><a
+                            href="https://www.pinkcube.nl/tassen-bedrukken/opvouwbare-boodschappentasjes.html"
+                            title="Opvouwbare boodschappentasjes bedrukken" class="panel-body">
+                        <div class="cat-img"><img
+                                    src="https://www.pinkcube.nl/media/catalog/product/cache/9/small_image/275x/9df78eab33525d08d6e5fb8d27136e95/o/p/opvouwbare-boodschappentas-met-unieke-opvouwfunctie-navy-532c-deco.png"
+                                    alt="Opvouwbare boodschappentasjes bedrukken????" height="90"></div>
+                        <div class="sub-info">
+                            <div class="align-middle"><span class="name">Opvouwbare boodschappentasjes</span> <span
+                                        class="sub-caption"> <ul> <li>Al vanaf 25 stuks</li> </ul> </span></div>
+                        </div>
+                    </a>
+                </div>
+                <div class="raleted_pro_detailds"><a
+                            href="https://www.pinkcube.nl/tassen-bedrukken/opvouwbare-boodschappentasjes.html"
+                            title="Opvouwbare boodschappentasjes bedrukken" class="panel-body">
+                        <div class="cat-img"><img
+                                    src="https://www.pinkcube.nl/media/catalog/product/cache/9/small_image/275x/9df78eab33525d08d6e5fb8d27136e95/o/p/opvouwbare-boodschappentas-met-unieke-opvouwfunctie-navy-532c-deco.png"
+                                    alt="Opvouwbare boodschappentasjes bedrukken????" height="90"></div>
+                        <div class="sub-info">
+                            <div class="align-middle"><span class="name">Opvouwbare boodschappentasjes</span> <span
+                                        class="sub-caption"> <ul> <li>Al vanaf 25 stuks</li> </ul> </span></div>
+                        </div>
+                    </a>
+                </div>
+                <div class="raleted_pro_detailds"><a
+                            href="https://www.pinkcube.nl/tassen-bedrukken/opvouwbare-boodschappentasjes.html"
+                            title="Opvouwbare boodschappentasjes bedrukken" class="panel-body">
+                        <div class="cat-img"><img
+                                    src="https://www.pinkcube.nl/media/catalog/product/cache/9/small_image/275x/9df78eab33525d08d6e5fb8d27136e95/o/p/opvouwbare-boodschappentas-met-unieke-opvouwfunctie-navy-532c-deco.png"
+                                    alt="Opvouwbare boodschappentasjes bedrukken????" height="90"></div>
+                        <div class="sub-info">
+                            <div class="align-middle"><span class="name">Opvouwbare boodschappentasjes</span> <span
+                                        class="sub-caption"> <ul> <li>Al vanaf 25 stuks</li> </ul> </span></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>';
+        $content = '
+<div class="content-below" >
     <div class="container">
         <div class="view-hometext-mobile">
             <a href="javascript:;">Bekijk meer info over tassen</a>
@@ -98,9 +178,7 @@ class InstallData implements InstallDataInterface
     </div>
 </div>'
         ;
-
         $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
-
         $eavSetup->addAttribute(
             \Magento\Catalog\Model\Category::ENTITY,
             'is_landing', [
@@ -119,7 +197,7 @@ class InstallData implements InstallDataInterface
         $cmsBlockData = [
             'title' => 'Landing Page',
             'identifier' => 'Landing-page',
-            'content' => '<p>{{block class="Ecentura\LandingPage\Block\Landing\Custom"  name ="landing-page" template="Ecentura_LandingPage::custom.phtml"}} </p>'.$content,
+            'content' => $banner.'<p>{{block class="Ecentura\LandingPage\Block\Landing\Custom"  name ="landing-page" template="Ecentura_LandingPage::custom.phtml"}} </p>'.$content,
             'is_active' => 1,
             'stores' => [0],
             'sort_order' => 0
